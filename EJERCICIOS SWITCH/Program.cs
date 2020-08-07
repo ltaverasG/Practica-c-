3c43daf37d72f544e4cbf316d9a4f584e5e80b78
ltaverasG <64043686+ltaverasG@users.noreply.github.com>
2020-08-06 17:18:49 -1100

    practica videos

---
+++ EJERCICIOS SWITCH/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EJERCICIOS_SWITCH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elige un medio de transporte: coche,tren,avion ");
+            Console.WriteLine();
+
+            string medioTransporte = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (medioTransporte )
+            {
+                case "coche":
+                    Console.WriteLine("Velocidad media: 100 km/h");
+                    break;
+
+                case "tren":
+                    Console.WriteLine("Velocidad media: 250km/h");
+                        break;
+                case "avion":
+                    Console.WriteLine("Velocidad media: 800km/h");
+                    break;
+
+                default:
+                    Console.WriteLine("Transporte no contemplado");
+                    break;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ha terminado el programa.");
+
+
+        }
+    }
+}
