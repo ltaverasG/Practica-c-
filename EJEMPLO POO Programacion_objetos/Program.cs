e275f3cacf909a682f6b916f9c8feb1a3bbfe999
ltaverasG <64043686+ltaverasG@users.noreply.github.com>
2020-08-10 00:21:25 -1100

    EJ: PROGRAMACIO ORIENTADO A OBJETOS

---
+++ EJEMPLO POO Programacion_objetos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EJEMPLO_POO_Programacion_objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo; // creacion de objeto tipo circulo 
+            miCirculo = new Circulo(); // iniciacion de variable // nuevo objeto 
+
+            Circulo TuCirculo = new Circulo();// creacion de un objeto circulo 
+
+            miCirculo.CalculoArea(5);
+
+            Console.WriteLine(miCirculo.CalculoArea (5));
+
+            Console.WriteLine(TuCirculo.CalculoArea(9)); 
+        }
+    }
+    class Circulo
+    {
+        
+        const  double PI = 3.1416; // PROPIEDAD CLASE CIRCULO / O / CAMPOS DE CLASE 
+
+       public  double  CalculoArea (int radio)      // metodo de clase // public para que se pueda usar en ontra clase 
+       {
+         return PI * radio * radio ;
+       }
+    }
+}
