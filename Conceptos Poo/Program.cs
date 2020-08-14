875d371cc8052a98c958d37b85e41ae6e0253832
ltaverasG <64043686+ltaverasG@users.noreply.github.com>
2020-08-13 23:21:58 -1100

    arrays

---
+++ Conceptos Poo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //   sintaxis basica 
+            /*
+             * 
+          int[] edades;
+
+          edades = new int[4];
+
+          edades[0] = 15;
+          edades[1] = 28;
+          edades[2] = 18;
+          edades[3] = 19;
+
+          Console.WriteLine((edades [3])); */
+
+            int[] edades = { 15, 27, 19, 80 };
+            Console.WriteLine((edades [1]));
+ 
+
+        }
+    }
+}
